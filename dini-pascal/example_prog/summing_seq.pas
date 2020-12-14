b4bb44ba667e6ca1f3cc8dc6fda026ce8aad8dc8
Stackofsugar <digno.christopher@gmail.com>
2020-12-14 18:00:10 +0700

    added file

--- dini-pascal/example_prog/summing_seq.pas
+++ dini-pascal/example_prog/summing_seq.pas
@@ -5,12 +5,21 @@
 program summing_seq;
 
 var
-    seq_length : Integer;
+    seq_length, seq_sum, i : Integer;
     seq_array : array of Integer;
 
 begin
     Write('How long is your sequence > ');
     ReadLn(seq_length);
-
     SetLength(seq_array, seq_length);
+
+    seq_sum := 0;
+    for i := 0 to (seq_length - 1) do
+    begin
+        Write('Enter ', (i + 1), ' th element: ');
+        ReadLn(seq_array[i]);
+        seq_sum := seq_sum + seq_array[i];
+    end;
+
+    WriteLn('The sum is: ', seq_sum);
 end.
